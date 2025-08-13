b02ac39592440df3d416dba7bd48e2b08d2e0919
Prem Kumar <prem-k@hcltech.com>
2025-08-14 03:11:41 +0530

    minor updates

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,20 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: ['react-native-reanimated/plugin'] 
+  plugins: [
+    ['module-resolver', {
+      root: ['./'],
+      extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+      alias: {
+        '@src': './src',
+        '@components': './src/components',
+        '@screens': './src/screens',
+        '@navigation': './src/navigation',
+        '@hooks': './src/hooks',
+        '@utils': './src/utils',
+        '@services': './src/services',
+        '@assets': './src/assets'
+      }
+    }],
+    'react-native-reanimated/plugin'
+  ]
 };
---
+++ react-native.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  project: {
+    ios: {},
+    android: {}
+  },
+  assets: ['./src/assets/fonts/'],
+  dependencies: {
+    // Example of disabling autolinking for a platform:
+    // 'react-native-some-lib': { platforms: { android: null } }
+  }
+};
